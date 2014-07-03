df0b37bbe5a025471ca9f6ca08ee7bf2b1121562
s-nlf-fh <s.nlf.fh@gmail.com>
2014-07-03 22:13:44 +0900

    spirit

---
+++ boost_spirit_test0/boost_spirit_test0/Source.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <boost/fusion/tuple.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix.hpp>
+
+using namespace std;
+using namespace boost;
+using namespace boost::spirit;
+
+//
+// 例3: 少しちゃんとした例。数式を計算します
+//	 文法定義
+//	   expr ::= term ('+' term | '-' term)*
+//	   term ::= fctr ('*' fctr | '/' fctr)*
+//	   fctr ::= int | '(' expr ')'
+//	 開始記号
+//	   expr
+//
+template<typename Iterator>
+struct calc	: qi::grammar<Iterator, int(), ascii::space_type>
+{
+	qi::rule<Iterator, int(), ascii::space_type> expr, term, fctr;
+
+	calc() : calc::base_type(expr)
+	{
+		expr = term[_val = _1] >> *(('+' >> term[_val += _1]) | ('-' >> term[_val -= _1]));
+		term = fctr[_val = _1] >> *(('*' >> fctr[_val *= _1]) | ('/' >> fctr[_val /= _1]))[&Oops];
+		fctr = int_ | '(' >> expr >> ')';
+	}
+
+	// 計算ついでに、* と / を見ると意味もなく Oops! と叫ぶコード
+	static void Oops()
+	{
+		cout << "Oops!" << endl;
+	}
+};
+
+int main()
+{
+	for (string str; getline(cin, str) && str.size() > 0;)
+	{
+		calc<string::iterator> c;
+		int result = -1;
+
+		string::iterator it = str.begin();
+		if (qi::phrase_parse(it, str.end(), c, ascii::space, result))
+			cout << result << endl;
+	}
+}
